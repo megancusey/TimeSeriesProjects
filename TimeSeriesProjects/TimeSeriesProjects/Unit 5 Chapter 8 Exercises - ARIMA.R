139552de09b380dc6ca1aeab5bddb969c15ca4d4
Megan Cusey <cuseymegan@gmail.com>
2019-09-22 15:23:05 -0500

    Add Chapter 8 HW

---
+++ TimeSeriesProjects/TimeSeriesProjects/Unit 5 Chapter 8 Exercises - ARIMA.R
@@ -0,0 +1,32 @@
+## Chapter 8 Exercises 1, 2, 3, and 5.
+
+## 1. Figure 8.31 shows the ACFs for 36 random numbers,
+## 360 random numbers and 1,000 random numbers.
+
+## a. Explain the differences amoung these figures. Do
+## they all indicate that the data are white noise?
+
+## b. Why are the critical values at differrent distances
+## from the mean of zero? Why are the autocorrelations 
+## different in each figure when they each refer to white 
+## noise?
+
+## 2. A classic example of a non-stationary series is the 
+## daily closing IBM stock price series (data set ibmclose).
+## Use R to plot the daily closing prices for IBM stock
+## and the ACF and PACF. Explain how each plot shows the series
+## is non-stationary and should be differenced.
+
+## 3. For the following series, find an appropriate Box-Cox
+## transformation and order of differencing in order to obtain
+## stationary data.
+
+## a. usnetelec
+## b. usgdp
+## c. mcopper
+## d. enplanements
+## e. visitors
+
+## 5. For your retail data (from Exercise 3 in Section 2.10), find
+## the appropriate order of differencing (after transformation if
+## necessary) to obtain stationary data.
